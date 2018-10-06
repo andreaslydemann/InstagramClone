fcbef0203964b3ffb616ebc98509dea6d527a7c8
Andreas Lüdemann <andreaslydemann@gmail.com>
2018-10-06 12:58:13 +0200

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Andreas Lüdemann on 06/10/2018.
+//  Copyright © 2018 Andreas Lüdemann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
