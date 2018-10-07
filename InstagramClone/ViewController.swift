df339a72f6a9b9fa7ceb13d9299fcb641f2b2659
Andreas Lüdemann <andreaslydemann@gmail.com>
2018-10-07 21:21:07 +0200

    Implement image picker on login screen

--- InstagramClone/ViewController.swift
+++ InstagramClone/ViewController.swift
@@ -9,13 +9,37 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(named: "plus_photo")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
         return button
     }()
+    
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[.editedImage] as? UIImage {
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width/2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        
+        dismiss(animated: true, completion: nil)
+    }
     
     let emailTextField: UITextField = {
         let tf = UITextField()
@@ -31,8 +55,8 @@ class ViewController: UIViewController {
     @objc func handleTextINputChange() {
         let isFormValid =
             emailTextField.text?.count ?? 0 > 0 &&
-            usernameTextField.text?.count ?? 0 > 0 &&
-            passwordTextField.text?.count ?? 0 > 0
+                usernameTextField.text?.count ?? 0 > 0 &&
+                passwordTextField.text?.count ?? 0 > 0
         
         if isFormValid {
             signUpButton.isEnabled = true
@@ -86,15 +110,47 @@ class ViewController: UIViewController {
         guard let username = usernameTextField.text, username.count > 0 else { return }
         guard let password = passwordTextField.text, password.count > 0 else { return }
         
-        Auth.auth().createUser(withEmail: email, password: password, completion: { (authDataResult: AuthDataResult?, error: Error?) in
-            
+        Auth.auth().createUser(withEmail: email, password: password) { (authDataResult: AuthDataResult?, error: Error?) in
             if let err = error {
                 print("Failed to create user:", err)
                 return
             }
             
             print("Successsfully created user:", authDataResult?.user.uid ?? "")
-        })
+            
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            guard let uploadData = image.jpegData(compressionQuality: 0.3) else { return }
+            
+            let filename = NSUUID().uuidString
+            let storageRef = Storage.storage().reference().child("profile_images").child(filename)
+            
+            storageRef.putData(uploadData, metadata: nil) { (metadata, err) in
+                if let err = err {
+                    print("Failed to upload profile image:", err)
+                    return
+                }
+                
+                storageRef.downloadURL { (url, err) in
+                    guard let profileImageUrl = url?.absoluteString else { return }
+                    
+                    print("Successsfully uploaded profile image:", profileImageUrl)
+                    
+                    guard let uid = authDataResult?.user.uid else { return }
+                    
+                    let dictionaryValues = ["username": username, "profileImageUrl": profileImageUrl]
+                    let values = [uid: dictionaryValues]
+                    
+                    Database.database().reference().child("users").updateChildValues(values) { (err, ref) in
+                        if let err = err {
+                            print("Failed to save user info into db:", err)
+                            return
+                        }
+                        
+                        print("Sucessfully saved user info to db")
+                    }
+                }
+            }
+        }
     }
     
     override func viewDidLoad() {
@@ -103,14 +159,14 @@ class ViewController: UIViewController {
         view.addSubview(plusPhotoButton)
         view.addSubview(emailTextField)
         
-        plusPhotoButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 40, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 140, height: 140)
-        plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusPhotoButton.anchor(top: view.topAnchor, leading: nil, bottom: nil, trailing: nil,
+                               padding: .init(top: 40, left: 0, bottom: 0, right: 0), size: .init(width: 140, height: 140))
+        plusPhotoButton.anchorCenter(to: view, horizontally: true, vertically: false)
         
         setupInputFields()
     }
     
     fileprivate func setupInputFields() {
-        
         let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
         
         stackView.distribution = .fillEqually
@@ -118,40 +174,51 @@ class ViewController: UIViewController {
         stackView.spacing = 10
         
         view.addSubview(stackView)
+        // [redView, blueView].forEach { view.adSubview($0) }
         
-        stackView.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor,
-                         paddingTop: 20, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 200)
+        stackView.anchor(top: plusPhotoButton.bottomAnchor, leading: view.leftAnchor, bottom: nil, trailing: view.rightAnchor,
+                         padding: .init(top: 20, left: 40, bottom: 0, right: 40), size: .init(width: 0, height: 200))
     }
 }
 
 extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, right: NSLayoutXAxisAnchor?,
-                paddingTop: CGFloat, paddingLeft: CGFloat, paddingBottom: CGFloat, paddingRight: CGFloat, width: CGFloat, height: CGFloat) {
+    
+    func anchorCenter(to view: UIView, horizontally: Bool = false, vertically: Bool = false) {
+        if horizontally {
+            centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        }
         
+        if vertically {
+            centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        }
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?,
+                trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
         translatesAutoresizingMaskIntoConstraints = false
         
         if let top = top {
-            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
         }
         
-        if let left = left {
-            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        if let leading = leading {
+            leftAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
         }
         
         if let bottom = bottom {
-            bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+            bottomAnchor.constraint(equalTo: bottom, constant: padding.bottom).isActive = true
         }
         
-        if let right = right {
-            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        if let trailing = trailing {
+            rightAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
         }
         
-        if width != 0 {
-            widthAnchor.constraint(equalToConstant: width).isActive = true
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
         }
         
-        if height != 0 {
-            heightAnchor.constraint(equalToConstant: height).isActive = true
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
         }
     }
 }
