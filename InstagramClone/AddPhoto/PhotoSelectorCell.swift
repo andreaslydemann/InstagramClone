62b7761dd02bcab4bbf4864ae28680afea2f58cc
Andreas Lüdemann <andreaslydemann@gmail.com>
2018-10-23 22:13:59 +0200

    Fetch Photos into PhotoSelector

---
+++ InstagramClone/AddPhoto/PhotoSelectorCell.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoSelectorCell.swift
+//  InstagramClone
+//
+//  Created by Andreas Lüdemann on 23/10/2018.
+//  Copyright © 2018 Andreas Lüdemann. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorCell: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        
+        backgroundColor = .brown
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: 0), size: .init(width: 0, height: 0))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
